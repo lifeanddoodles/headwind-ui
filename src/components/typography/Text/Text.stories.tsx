aaedab1aba793b3d3ff2d3d3860d3342b00d4e94
“Sandra <sandravargasgro@gmail.com>
2025-07-02 20:51:48 -0700

    Delete Storybook boilerplate files. Update Text story.

--- src/components/typography/Text/Text.stories.tsx
+++ src/components/typography/Text/Text.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { sizeOptions, toneOptions, weightOptions } from "../utils";
 import { Text, textTags } from "./Text";
 
 const meta: Meta<typeof Text> = {
@@ -6,35 +7,55 @@ const meta: Meta<typeof Text> = {
   component: Text,
   argTypes: {
     as: {
-      control: { type: "select", options: textTags },
-      description: "HTML text-related element tag (h1 - h6)",
+      control: { type: "select" },
+      options: textTags,
+      description: "HTML text-related element tag (p, span, strong, etc.)",
+    },
+    size: {
+      control: { type: "select" },
+      options: sizeOptions,
+      description: "Text size",
+    },
+    weight: {
+      control: { type: "select" },
+      options: weightOptions,
+      description: "Font weight",
+    },
+    tone: {
+      control: { type: "select" },
+      options: toneOptions,
+      description: "Text color tone",
     },
     children: {
       control: "text",
       description: "Text content",
     },
   },
+  args: {
+    children: "Default text tag (p)",
+    size: "base",
+    weight: "normal",
+    tone: "default",
+    as: "p",
+  },
+  tags: ["autodocs"],
 };
 
 export default meta;
 type Story = StoryObj<typeof Text>;
 
-export const Default: Story = {
-  args: {
-    children: "Default text tag (p)",
-  },
-};
+export const Default: Story = {};
 
 export const Span: Story = {
   args: {
     as: "span",
-    children: "This is a span",
+    children: "This is a span element",
   },
 };
 
 export const Strong: Story = {
   args: {
     as: "strong",
-    children: "This is strong text",
+    children: "This is a strong element",
   },
 };
